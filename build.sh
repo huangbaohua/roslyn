ad793f0964b5ab812fd7e0cb7d70d7c904710e92
Mike Krüger <mkrueger@xamarin.com>
2014-07-16 10:51:19 +0200

    Added build script.

---
+++ build.sh
@@ -0,0 +1,5 @@
+find . -name "obj" | xargs rm -fdr
+mkdir -p Src/packages/Microsoft.Net.ToolsetCompilers.0.7.4032713-beta/build
+echo "<Project DefaultTargets=\"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\"><PropertyGroup><DisableRoslyn>true</DisableRoslyn><CscToolPath Condition=\" '\$(OS)' == 'Windows_NT'\">$(MSBuildThisFileDirectory)..\\tools</CscToolPath><CscToolExe Condition=\" '\$(OS)' == 'Windows_NT'\">rcsc2.exe</CscToolExe><VbcToolPath>\$(MSBuildThisFileDirectory)..\tools</VbcToolPath><VbcToolExe>rvbc2.exe</VbcToolExe></PropertyGroup></Project>" >Src/packages/Microsoft.Net.ToolsetCompilers.0.7.4032713-beta/build/Microsoft.Net.ToolsetCompilers.props
+xbuild Src/Compilers/CSharp/csc/csc.csproj
+xbuild Src/Workspaces/CSharp/CSharpWorkspace.csproj
