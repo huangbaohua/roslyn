13173284ffafd7d00f108843632b0ea1c0dd6d57
Mike Krüger <mkrueger@xamarin.com>
2014-11-12 10:56:14 +0100

    Updated build.sh.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 find . -name "obj" | xargs rm -fdr
-mkdir -p Src/packages/Microsoft.Net.ToolsetCompilers.0.7.4032713-beta/build
+mkdir -p packages/Microsoft.Net.ToolsetCompilers.0.7.4101501-beta/build
 echo "<Project DefaultTargets=\"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">
   <PropertyGroup>
     <DisableRoslyn>true</DisableRoslyn>
@@ -8,6 +8,8 @@ echo "<Project DefaultTargets=\"Build\" xmlns=\"http://schemas.microsoft.com/dev
     <VbcToolPath>$(MSBuildThisFileDirectory)..\tools</VbcToolPath>
     <VbcToolExe>vbc2.exe</VbcToolExe>
   </PropertyGroup>
-</Project>" >packages/Microsoft.Net.ToolsetCompilers.0.7.4100302-beta/build/Microsoft.Net.ToolsetCompilers.props
+</Project>" >packages/Microsoft.Net.ToolsetCompilers.0.7.4101501-beta/build/Microsoft.Net.ToolsetCompilers.props
+xbuild Src/Tools/Source/FakeSign/FakeSign.csproj
+xbuild Src/Compilers/CSharp/csc/csc.csproj
 xbuild /p:Configuration=Debug Src/Compilers/CSharp/csc/csc.csproj
 xbuild /p:Configuration=Debug ./Src/Workspaces/CSharp/Desktop/CSharpWorkspace.Desktop.csproj
