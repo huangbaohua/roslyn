836930fc60afc33deb617638258c5d7bf666a8ea
Mike Krüger <mkrueger@xamarin.com>
2014-11-25 13:56:35 +0100

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 find . -name "obj" | xargs rm -fdr
-mkdir -p packages/Microsoft.Net.ToolsetCompilers.0.7.4101501-beta/build
+TOOLSET_DIR=packages/Microsoft.Net.ToolsetCompilers.0.7.4101501-beta/build
+mkdir -p $TOOLSET_DIR
 echo "<Project DefaultTargets=\"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">
   <PropertyGroup>
     <DisableRoslyn>true</DisableRoslyn>
-    <CscToolPath Condition=\" '$(OS)' == 'Windows_NT'\">$(MSBuildThisFileDirectory)..\tools</CscToolPath>
-    <CscToolExe Condition=\" '$(OS)' == 'Windows_NT'\">csc2.exe</CscToolExe>
-    <VbcToolPath>$(MSBuildThisFileDirectory)..\tools</VbcToolPath>
+    <CscToolPath Condition=\" '\$(OS)' == 'Windows_NT'\">\$(MSBuildThisFileDirectory)..\tools</CscToolPath>
+    <CscToolExe Condition=\" '\$(OS)' == 'Windows_NT'\">csc2.exe</CscToolExe>
+    <VbcToolPath>\$(MSBuildThisFileDirectory)..\tools</VbcToolPath>
     <VbcToolExe>vbc2.exe</VbcToolExe>
   </PropertyGroup>
-</Project>" >packages/Microsoft.Net.ToolsetCompilers.0.7.4101501-beta/build/Microsoft.Net.ToolsetCompilers.props
+</Project>" >$TOOLSET_DIR/Microsoft.Net.ToolsetCompilers.props
 xbuild Src/Tools/Source/FakeSign/FakeSign.csproj
-xbuild Src/Compilers/CSharp/csc/csc.csproj
 xbuild /p:Configuration=Debug Src/Compilers/CSharp/csc/csc.csproj
 xbuild /p:Configuration=Debug ./Src/Workspaces/CSharp/Desktop/CSharpWorkspace.Desktop.csproj
